ca00cd4258b0c7f1469b5a6ed09367d64b2be023
Phyllis Kan <pyk9q7@gmail.com>
2019-09-02 17:54:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  def initialized(title)
+  def initialize(title)
     @title = title
   end
 
@@ -8,8 +8,20 @@ class Book
     @title
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
   end
 end
 
