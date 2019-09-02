77ca8c1b254c64774bef5f8a3f1bc9e283e1f291
Phyllis Kan <pyk9q7@gmail.com>
2019-09-02 18:18:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -11,30 +11,6 @@ class Shoe
     @brand
   end
 
-  # def color=(color)
-  #   @color = color
-  # end
-  #
-  # def color
-  #   @color
-  # end
-  #
-  # def size=(size)
-  #   @size = size
-  # end
-  #
-  # def size
-  #   @size
-  # end
-  #
-  # def material=(material)
-  #   @material = material
-  # end
-  #
-  # def material
-  #   @material
-  # end
-
   def condition=(condition)
     @condition = condition
   end
